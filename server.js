4ee48f2384660c439cb993a1a211e9b30d55ebd0
Jade Hanonge <jade.hanonge@etu.u-bordeaux.fr>
2025-10-29 14:35:18 +0100

    initialisation of the server

---
+++ server.js
@@ -0,0 +1,18 @@
+// initialitiation server
+const express = require('express');
+const cors = require('cors');
+const projectsRoutes = require('./backend/routes/app.routes');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+
+//utiliser les routes définies dans routes.js
+app.use('/api', projectsRoutes);
+
+//Lancer le serveur
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
